6ef2d5497b908e5647e8bd4f099a3752b7b59ec2
serega.dp <serega.dp@gmail.com>
2021-09-05 00:36:35 +0300

    feat: add null safety

--- lib/flutter_zendesk.dart
+++ lib/flutter_zendesk.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 class FlutterZendesk {
   static const MethodChannel _channel = const MethodChannel('flutter_zendesk');
 
-  static Future<String> initiateZendesk({@required Map<String, dynamic> params}) async {
+  static Future<String> initiateZendesk({required Map<String, dynamic> params}) async {
     return await _channel.invokeMethod('initiate', params);
   }
 }
